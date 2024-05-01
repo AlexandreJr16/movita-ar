1d2c70c46010e11af8c686a5560776f595893997
AlexandreJr16 <alexandrep.souzzza@gmail.com>
2024-05-01 13:47:51 -0400

    Removenod gambiarra

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-// vite.config.js
 export default {
-  // don't allow automatic port switching as we need to know the port
   server: {
     strictPort: true,
     host: "0.0.0.0",
   },
   root: "src",
   build: {
-    outDir: "../build",
+    outDir: "../dist", // Alterado para "dist"
   },
   publicDir: "../public",
 };
